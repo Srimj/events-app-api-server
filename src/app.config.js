0eca6297ebf35f397b75ea140d8a1bba592938ba
Srimj <srishailjannur@gmail.com>
2022-09-01 06:52:07 +0000

    New change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Srishail';
+    team = 'New feature';
     version = '1.0.0';
 }
 module.exports = AppConfig;
